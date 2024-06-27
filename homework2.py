8cda1f010b390a57f20ed4bdc1004757ab381fff
MADFLYKAH <kanbt@mail.ru>
2024-06-27 14:48:07 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+HW_count = 12
+Hour_count = 1.5
+Cource_name = 'Python'
+One_HW_time = Hour_count / HW_count
+print("Курс:", Cource_name,", ", "всего задач:", HW_count, ", ", "затрачено часов:", Hour_count, ", ", "среднее время выполнения:", One_HW_time, sep="")
